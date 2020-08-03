7805b4844414e287549b76ca346253079a8a69a9
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-08-03 21:44:00 +0200

    add meal model and refactor project folder

--- lib/screens/category_meal_screen.dart
+++ lib/screens/category_meal_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealScreen extends StatelessWidget {
   // final String id;
   // final String title;
@@ -13,12 +15,18 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('Receipes for the category'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
